74296925dce2ea3978a5957ce1cf99a636708a2d
Andriy Trubchanin <magtuxgit@gmail.com>
2018-04-25 15:08:43 +0300

    array of annotations

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     let regionRadius: CLLocationDistance = 1000
+    var artworks: [Artwork] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,17 +15,37 @@ class ViewController: UIViewController {
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(location: initialLocation)
         
+        /*
         // show artwork on map
         let artwork = Artwork(title: "King David Kalakaua",
                               locationName: "Waikiki Gateway Park",
                               discipline: "Sculpture",
                               coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
         mapView.addAnnotation(artwork)
+        */
+        loadInitialData()
+        mapView.addAnnotations(artworks)
     }
     
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func loadInitialData() {
+        guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json") else { return }
+        
+        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+        
+        guard
+            let data = optionalData,
+            let json = try? JSONSerialization.jsonObject(with: data),
+            let dictionary = json as? [String: Any],
+            let works = dictionary["data"] as? [[Any]]
+            else { return }
+        
+        let validWorks = works.flatMap { Artwork(json: $0) }
+        artworks.append(contentsOf: validWorks)
     }
 }
 
